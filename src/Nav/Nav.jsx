81500d2207bdc28513396e3e55b8a032ee448023
arrogance231 <alchemyofjhine@gmail.com>
2024-03-25 15:58:27 +0800

    navbar

--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Navlinks } from '../constants'
 
 const Nav = () => {
   return (
-    <div  className='w-full overflow-hidden bg-black h-5'>
-        Hello
+    <div  className='w-full overflow-hidden lg:h-[100px] sm:h-[80px] bg-blue-500 '>
+        <ul className='flex overflow-hidden mx-4 p-8 '>
+          <li>
+            <a href="#GoodNovel">
+              GoodNovel
+            </a>
+          </li>
+          {Navlinks.map((item) => (
+            <li className='px-6'>
+              <a href={item.href} className=' relative flex'>
+                {item.label}
+              </a>
+            </li>
+          ))}
+            <div className='h-[30px] w-[100px] bg-black z-0 '>
+              <div className='flex z-10 bg-white h-auto w-[80px] '>
+                <img src="" alt="" />
+                <h1 className=''>
+                 Search
+                </h1>
+              </div>
+          </div>
+          </ul>
+          
     </div>
   )
 }
---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+export const Navlinks = [
+    { href: "#Home", label: "Home"},
+    { href: "#Browse", label: "Browse"},
+    { href: "#Ranking", label: "Ranking"},
+    { href: "#Create", label: "Create"},
+    { href: "#WriterBenefit", label: "WriterBenefit"},
+    { href: "#Contest", label: "Contest"},
+]
+
