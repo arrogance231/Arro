8f3ede3bcbbeb9ba396212b65f4433ec7f0c1a5b
arrogance231 <alchemyofjhine@gmail.com>
2024-03-25 21:59:57 +0800

    Finished navbar template, still trying to understand how to make it responsive

--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
-import { Navlinks } from '../constants'
+import React from 'react';
+import { Navlinks } from '../constants';
+const ImagePath = '/images/search.png';
 
 const Nav = () => {
   return (
-    <div  className='w-full overflow-hidden lg:h-[100px] sm:h-[80px] bg-blue-500 '>
+    <div  className='w-[100%] overflow-hidden lg:h-[20%] sm:h-[15%px] bg-blue-500 flex items-center relative'>
         <ul className='flex overflow-hidden mx-4 p-8 '>
-          <li>
-            <a href="#GoodNovel">
-              GoodNovel
+          <li className='p-1'>
+            <a href="#GoodNovel" className='border-4  border-green-500 bg-red-200 mr-[10px] font-bold'>
+              Zetro Novels
             </a>
           </li>
           {Navlinks.map((item) => (
             <li className='px-6'>
-              <a href={item.href} className=' relative flex'>
+              <a href={item.href} className=' relative flex border-4 border-gray-300 bg-white'>
                 {item.label}
               </a>
             </li>
           ))}
-            <div className='h-[30px] w-[100px] bg-black z-0 '>
-              <div className='flex z-10 bg-white h-auto w-[80px] '>
-                <img src="" alt="" />
+            <div className='h-[30px] w-[100px] bg-black z-0 flex justify-center items-center rounded-md bg-clip-text mx-[50px]'>
+              <div className='flex z-10 bg-white h-auto w-[80px] rounded-md'>
+                <img src={ImagePath} alt=""  className='h-auto w-5'/>
                 <h1 className=''>
                  Search
                 </h1>
